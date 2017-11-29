4f9711c061729c1d224abedac05e39de0868891c
Daniel Griffin <dcompgriff@gmail.com>
2017-11-29 15:39:16 -0600

    Added GenerativeBayesNet.py, and initial implementation of MLE based net.

---
+++ GenerativeBayesNet.py
@@ -0,0 +1,197 @@
+'''
+This code implements a hybrid, discrete continuous generative bayes net.
+This code assumes the bayes net structure is nearly markovian, with directed
+links between nodes representing each time point, and a directed link
+between the class node, and all data nodes. This network can be thought of
+in many ways including a special "Tree-Augmented" bayes net, or a special
+kind of Hidden Markov Model with the restriction that there is only one
+unobserved state (The class node), which is tied to all of the observation
+data.
+
+'''
+import numpy as np
+import pandas as pd
+import cvxpy as cvx
+from sklearn.datasets import load_iris
+from sklearn.svm import LinearSVC
+import sklearn.svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+import random
+
+'''
+
+'''
+class BayesNet:
+
+    def __init__(self):
+        self._dag = {}
+
+    def learn(self, X, y):
+        pass
+
+    def probability(self, x, y=1):
+        pass
+
+
+'''
+This function reads in a data file, and parses out the data set X,
+and class label set Y. It the builds a conditional gaussian
+distributed, markovian based bayes net, where the class label
+represents a discrete valued node in the graph, and each time
+point represents a continuous, gaussian distributed node that
+only depends on (Aka has parents) the previous value in time,
+and the current class label value.
+'''
+def buildBayesNet():
+    print("Loading data...")
+    data = pd.read_csv("./Train_Phone-Acc-nexus4_1-a.csv")
+    print("Done!")
+
+    # Parse data and make bike vs not-biking classification using an SVM.
+    # Note: I'm assuming a window width of 500
+    print("Finding time series windows indexes for each class kind...")
+    previousClassLabel = str(data.get_value(data.index[0], 'gt'))
+    pos = 0
+    y = []
+    X = []
+    window = 500
+    while pos < data.shape[0]:
+        # Make y label.
+        if str(data.iloc[pos]['gt']) == 'sit':
+            y.append(1)
+        else:
+            y.append(-1)
+
+        # Make X row.
+        X.append(data.iloc[pos:pos + window]['y'])
+
+        # Move to the next window
+        pos += window
+    print("Done!")
+
+    # Build and fit the Bayes Net.
+    # X is nx500, where each row represents a window
+    X = np.array(X)
+    # Y is nx1, belonging either to class -1 or +1
+    y = np.array(y)
+
+    '''
+    Steps for MLE estimates for each node xj:
+    1) Filter rows in X to be only those that have the desired class, -1 or +1.
+    2) Calculate the prior probability for x0 as a gaussian/uniform distribution for each class.
+    3) Calculate xj's sample variance ssj.
+    4) Calculate xj's MLE estimate for aj using the hand derived equation.
+    5) Calculate xj's MLE estimate for bj by plugging the calculated aj into the derived equation.
+    6) Given the aj's, bj's, and ssj's for each class value, build the Bayes Net Structure.
+    '''
+
+    # 1)
+    X1 = X[y == 1]
+    X_1 = X[y == -1]
+
+    # 2)
+    x1mean = np.mean(X1[:, 0], dtype=np.float64)
+    x1ss = np.var(X1[:, 0], dtype=np.float64)
+    x_1mean = np.mean(X_1[:, 0], dtype=np.float64)
+    x_1ss = np.var(X_1[:, 0], dtype=np.float64)
+
+    # 3)
+    ss1 = np.var(X1, axis=0, dtype=np.float64)
+    ss_1 = np.var(X_1, axis=0, dtype=np.float64)
+
+    # 4)
+    n1 = X1.shape[0]
+    n_1 = X_1.shape[0]
+    a1 = np.zeros((X1.shape[1], 1))
+    a_1 = np.zeros((X_1.shape[1], 1))
+    # Calculate ai for class 1
+    for j in range(1, X1.shape[1]):
+        sumXj = np.sum(X1[:,j])
+        sumXj_1 = np.sum(X1[:, j-1])
+        M = -1*np.sum(X1[:, j]*X1[:, j-1])
+        K = -1*np.sum(X1[:, j-1]*X1[:, j-1])
+        L = (-1/n1)*sumXj*sumXj_1
+        P = (-1/n1)*sumXj_1*sumXj_1
+        a1[j] = (M + L) / (K + P)
+    # Calculate ai for class -1
+    for j in range(1, X_1.shape[1]):
+        sumXj = np.sum(X_1[:, j])
+        sumXj_1 = np.sum(X_1[:, j - 1])
+        M = -1 * np.sum(X_1[:, j] * X_1[:, j - 1])
+        K = -1 * np.sum(X_1[:, j - 1] * X_1[:, j - 1])
+        L = (-1 / n1) * sumXj * sumXj_1
+        P = (-1 / n1) * sumXj_1 * sumXj_1
+        a_1[j] = (M + L) / (K + P)
+
+    # 5)
+    b1 = np.zeros((X1.shape[1], 1))
+    b_1 = np.zeros((X_1.shape[1], 1))
+    # Calculate bi for class 1
+    for j in range(1, X1.shape[1]):
+        b1[j] = (-1/n1)*(np.sum(X1[:, j]))*(-1*a1[j]*np.sum(X1[:, j-1]))
+    # Calculate bi for class -1
+    for j in range(1, X_1.shape[1]):
+        b_1[j] = (-1 / n1) * (np.sum(X_1[:, j])) * (-1 * a_1[j] * np.sum(X_1[:, j - 1]))
+
+    # 6)
+    # Base probability of class y
+    py = {-1: X[y == -1].shape[0] / X.shape[0], 1: X[y == 1].shape[0] / X.shape[0]}
+    # List of dicts {-1: [aj, bj, ssj], 1: [aj, bj, ssj]}
+    nodeList = [{-1: [x_1mean, x_1ss], 1: [x1mean, x1ss]}]
+    for j in range(1, X.shape[1]):
+        nodeList.append({-1: [a_1[j], b_1[j], ss_1[j]], 1: [a1[j], b1[j], ss1[j]]})
+
+
+
+def main():
+    buildBayesNet()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
